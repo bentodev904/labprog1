b0793382c0fcf76ac4a3ada2e1db5d0039a6f05f
BentoBento <148278977+bentodev904@users.noreply.github.com>
2026-02-26 21:35:08 -0400

    Add files via upload

---
+++ teste.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World!\n\n\n\n\n\n\n\n");
+    printf("seu nome\n");
+    printf("Sistemas de Informacao\n");
+    printf("Universidade Federal da Grande Dourados");
+    return 0;
+}
